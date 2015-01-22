b953838152b79dcfc5e60580d9012455b965d6db
Eagle Flies <myeagleflies@gmail.com>
2015-01-22 18:15:09 +0100

    Increasing MIN_PROTO_VERSION

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@ extern const std::string CLIENT_DATE;
 static const int PROTOCOL_VERSION = 88880;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 60000;
+static const int MIN_PROTO_VERSION = 88880;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
